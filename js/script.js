32cb40f1d7af7bc00a4820e05d4bca236937831b
Kadu Magalhães <54483448+kdumagalhaes@users.noreply.github.com>
2020-06-28 15:30:22 -0300

    js and css improvements
    addition of 2 buttons

--- js/script.js
+++ js/script.js
@@ -3,17 +3,29 @@ const calculadora = document.getElementById('calculadora')
 const meuSalario = document.getElementById('meu-salario')
 const meuBonus = document.getElementById('meu-bonus')
 const mesesTrabalhados = document.getElementById('meses-trabalhados')
+const btnLimpar = document.getElementById('btn-limpar')
+const resultPLR = document.getElementById('meu-plr')
 
-calculadora.addEventListener('change', (e) => {
+calculadora.addEventListener('submit', (e) => {
     e.preventDefault()
     let valorSalario = meuSalario.value
     let valorBonus = meuBonus.value
     let quantMeses = mesesTrabalhados.value
     let parcelaBonus = (valorSalario * valorBonus) / 12 
     let meuPLR = parcelaBonus * quantMeses
-    const resultPLR = document.getElementById('meu-plr')
+    let plrConvertido = meuPLR.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
+
     resultPLR.innerHTML = `
-    <p>Meu PLR</p>
-    <p id="resultado">R$${meuPLR.toFixed(2)}</p>
+    <h2>Meu PLR*</h2>
+    <p id="resultado">${plrConvertido}</p>
+    <small>*Valor bruto.</small>
     `
+    calculadora.style.borderBottom = '1px solid var(--verde-sombreado)'
+
+
+})
+
+btnLimpar.addEventListener('click', () => {
+    resultPLR.innerHTML = ''
+    calculadora.style.borderBottom = 'none'
 })
